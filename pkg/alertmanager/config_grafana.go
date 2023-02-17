b1f209de57685c8629cce4e4516c333160f078f0
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-02-17 12:36:52 -0500

    implement email cli

--- pkg/alertmanager/config_grafana.go
+++ pkg/alertmanager/config_grafana.go
@@ -5,12 +5,22 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
-	"errors"
+	"mime/quotedprintable"
+	"net/textproto"
+
 	"fmt"
 	"io"
+	"mime/multipart"
+	"net"
 	"net/http"
+	"net/mail"
+	"net/smtp"
 	"net/url"
 	"path/filepath"
+	"time"
+
+	"github.com/grafana/alerting/templates"
+	"github.com/pkg/errors"
 
 	gklog "github.com/go-kit/log"
 	"github.com/go-kit/log/level"
@@ -152,7 +162,20 @@ func (g GrafanaWrapper) BuildIntegrationsMap(userID string, tenantDir string, ex
 		}
 
 		emailCli := func(info receivers.NotifierInfo) (receivers.EmailSender, error) {
-			return &emailSender{}, nil
+			emailTemplate, err := templates.NewEmailTemplate(templates.TemplateConfig{
+				AppUrl:                externalURL.String(),
+				BuildVersion:          "0.0.0-alpha",
+				BuildStamp:            "n/a",
+				EmailCodeValidMinutes: 0,
+			})
+			if err != nil {
+				return nil, errors.Wrap(err, "fail to initialize email templates")
+			}
+			return &emailSender{
+				conf:   g.conf.Global,
+				tmpl:   emailTemplate,
+				logger: logger,
+			}, nil
 		}
 
 		integr, err := notify2.BuildReceiverIntegrations(grafana, grafanaTmpl, webhookCli, emailCli, store, func(ctx ...interface{}) logging.Logger {
@@ -280,224 +303,166 @@ func (w webhookSender) SendWebhook(ctx context.Context, cmd *receivers.SendWebho
 }
 
 type emailSender struct {
-	smarthost config.HostPort
-	tlsConfig *commoncfg.TLSConfig
-	logger    gklog.Logger
-	hello     string
+	conf   *config.GlobalConfig
+	tmpl   *templates.EmailTemplate
+	logger gklog.Logger
 }
 
 func (n emailSender) SendEmail(ctx context.Context, cmd *receivers.SendEmailSettings) (bool, error) {
-	// TODO here we will need to render another template the same way Grafana does
-	return false, errors.New("NOT IMPLEMENETED YET!") // TODO do something with it
-	/*
-		var (
-			c       *smtp.Client
-			conn    net.Conn
-			err     error
-			success = false
-		)
-		if n.smarthost.Port == "465" {
-			tlsConfig, err := commoncfg.NewTLSConfig(n.tlsConfig)
-			if err != nil {
-				return errors.Wrap(err, "parse TLS configuration")
-			}
-			if tlsConfig.ServerName == "" {
-				tlsConfig.ServerName = n.smarthost.Host
-			}
+	var (
+		c       *smtp.Client
+		conn    net.Conn
+		err     error
+		success = false
+		d       = net.Dialer{}
+	)
 
-			conn, err = tls.Dial("tcp", n.smarthost.String(), tlsConfig)
-			if err != nil {
-				return errors.Wrap(err, "establish TLS connection to server")
-			}
-		} else {
-			var (
-				d   = net.Dialer{}
-				err error
-			)
-			conn, err = d.DialContext(ctx, "tcp", n.smarthost.String())
-			if err != nil {
-				return errors.Wrap(err, "establish connection to server")
-			}
+	conn, err = d.DialContext(ctx, "tcp", n.conf.SMTPSmarthost.String())
+	if err != nil {
+		return true, errors.Wrap(err, "establish connection to server")
+	}
+
+	c, err = smtp.NewClient(conn, n.conf.SMTPSmarthost.Host)
+	if err != nil {
+		conn.Close()
+		return true, errors.Wrap(err, "create SMTP client")
+	}
+	defer func() {
+		// Try to clean up after ourselves but don't log anything if something has failed.
+		if err := c.Quit(); success && err != nil {
+			level.Warn(n.logger).Log("msg", "failed to close SMTP connection", "err", err)
 		}
-		c, err = smtp.NewClient(conn, n.smarthost.Host)
+	}()
+
+	if n.conf.SMTPHello != "" {
+		err = c.Hello(n.conf.SMTPHello)
 		if err != nil {
-			conn.Close()
-			return errors.Wrap(err, "create SMTP client")
+			return true, errors.Wrap(err, "send EHLO command")
 		}
-		defer func() {
-			// Try to clean up after ourselves but don't log anything if something has failed.
-			if err := c.Quit(); success && err != nil {
-				level.Warn(n.logger).Log("msg", "failed to close SMTP connection", "err", err)
-			}
-		}()
+	}
 
-		if n.hello != "" {
-			err = c.Hello(n.hello)
-			if err != nil {
-				return errors.Wrap(err, "send EHLO command")
-			}
-		}
+	// Skipping TLS for now
+	// // Global Config guarantees RequireTLS is not nil.
+	// if n.conf.SMTPRequireTLS {
+	// 	if ok, _ := c.Extension("STARTTLS"); !ok {
+	// 		return true, errors.Errorf("'require_tls' is true (default) but %q does not advertise the STARTTLS extension", n.conf.SMTPSmarthost)
+	// 	}
+	//
+	// 	tlsConf, err := commoncfg.NewTLSConfig(n.tlsConfig)
+	// 	if err != nil {
+	// 		return false, errors.Wrap(err, "parse TLS configuration")
+	// 	}
+	// 	if tlsConf.ServerName == "" {
+	// 		tlsConf.ServerName = n.conf.SMTPSmarthost.Host
+	// 	}
+	//
+	// 	if err := c.StartTLS(tlsConf); err != nil {
+	// 		return true, errors.Wrap(err, "send STARTTLS command")
+	// 	}
+	// }
 
-		// Global Config guarantees RequireTLS is not nil.
-		if *n.conf.RequireTLS {
-			if ok, _ := c.Extension("STARTTLS"); !ok {
-				return true, errors.Errorf("'require_tls' is true (default) but %q does not advertise the STARTTLS extension", n.conf.Smarthost)
-			}
+	// TODO skipping auth for now as we do not need it
 
-			tlsConf, err := commoncfg.NewTLSConfig(&n.conf.TLSConfig)
-			if err != nil {
-				return false, errors.Wrap(err, "parse TLS configuration")
-			}
-			if tlsConf.ServerName == "" {
-				tlsConf.ServerName = n.conf.Smarthost.Host
-			}
+	// if ok, mech := c.Extension("AUTH"); ok {
+	// 	auth, err := n.auth(mech)
+	// 	if err != nil {
+	// 		return true, errors.Wrap(err, "find auth mechanism")
+	// 	}
+	// 	if auth != nil {
+	// 		if err := c.Auth(auth); err != nil {
+	// 			return true, errors.Wrapf(err, "%T auth", auth)
+	// 		}
+	// 	}
+	// }
 
-			if err := c.StartTLS(tlsConf); err != nil {
-				return true, errors.Wrap(err, "send STARTTLS command")
-			}
-		}
-
-		if ok, mech := c.Extension("AUTH"); ok {
-			auth, err := n.auth(mech)
-			if err != nil {
-				return true, errors.Wrap(err, "find auth mechanism")
-			}
-			if auth != nil {
-				if err := c.Auth(auth); err != nil {
-					return true, errors.Wrapf(err, "%T auth", auth)
-				}
-			}
-		}
-
-		var (
-			tmplErr error
-			data    = notify.GetTemplateData(ctx, n.tmpl, as, n.logger)
-			tmpl    = notify.TmplText(n.tmpl, data, &tmplErr)
-		)
-		from := tmpl(n.conf.From)
-		if tmplErr != nil {
-			return false, errors.Wrap(tmplErr, "execute 'from' template")
-		}
-		to := tmpl(n.conf.To)
-		if tmplErr != nil {
-			return false, errors.Wrap(tmplErr, "execute 'to' template")
-		}
-
-		addrs, err := mail.ParseAddressList(from)
-		if err != nil {
-			return false, errors.Wrap(err, "parse 'from' addresses")
-		}
-		if len(addrs) != 1 {
-			return false, errors.Errorf("must be exactly one 'from' address (got: %d)", len(addrs))
-		}
-		if err = c.Mail(addrs[0].Address); err != nil {
-			return true, errors.Wrap(err, "send MAIL command")
-		}
-		addrs, err = mail.ParseAddressList(to)
+	addrs, err := mail.ParseAddressList(n.conf.SMTPFrom) // TODO in global config this can be a template!
+	if err != nil {
+		return false, errors.Wrap(err, "parse 'from' addresses")
+	}
+	if len(addrs) != 1 {
+		return false, errors.Errorf("must be exactly one 'from' address (got: %d)", len(addrs))
+	}
+	if err = c.Mail(addrs[0].Address); err != nil {
+		return true, errors.Wrap(err, "send MAIL command")
+	}
+	for _, a := range cmd.To {
+		addr, err := mail.ParseAddress(a)
 		if err != nil {
 			return false, errors.Wrapf(err, "parse 'to' addresses")
 		}
-		for _, addr := range addrs {
-			if err = c.Rcpt(addr.Address); err != nil {
-				return true, errors.Wrapf(err, "send RCPT command")
-			}
+		if err = c.Rcpt(addr.Address); err != nil {
+			return true, errors.Wrapf(err, "send RCPT command")
 		}
+	}
 
-		// Send the email headers and body.
-		message, err := c.Data()
-		if err != nil {
-			return true, errors.Wrapf(err, "send DATA command")
-		}
-		defer message.Close()
+	// Send the email headers and body.
+	message, err := c.Data()
+	if err != nil {
+		return true, errors.Wrapf(err, "send DATA command")
+	}
+	defer message.Close()
 
-		buffer := &bytes.Buffer{}
-		for header, t := range n.conf.Headers {
-			value, err := n.tmpl.ExecuteTextString(t, data)
-			if err != nil {
-				return false, errors.Wrapf(err, "execute %q header template", header)
-			}
-			fmt.Fprintf(buffer, "%s: %s\r\n", header, mime.QEncoding.Encode("utf-8", value))
-		}
+	buffer := &bytes.Buffer{}
+	// No headers for now
 
-		if _, ok := n.conf.Headers["Message-Id"]; !ok {
-			fmt.Fprintf(buffer, "Message-Id: %s\r\n", fmt.Sprintf("<%d.%d@%s>", time.Now().UnixNano(), rand.Uint64(), n.hostname))
-		}
+	// for header, t := range n.conf.Headers {
+	// 	value, err := n.tmpl.ExecuteTextString(t, data)
+	// 	if err != nil {
+	// 		return false, errors.Wrapf(err, "execute %q header template", header)
+	// 	}
+	// 	fmt.Fprintf(buffer, "%s: %s\r\n", header, mime.QEncoding.Encode("utf-8", value))
+	// }
+	//
+	// if _, ok := n.conf.Headers["Message-Id"]; !ok {
+	// 	fmt.Fprintf(buffer, "Message-Id: %s\r\n", fmt.Sprintf("<%d.%d@%s>", time.Now().UnixNano(), rand.Uint64(), n.hostname))
+	// }
 
-		multipartBuffer := &bytes.Buffer{}
-		multipartWriter := multipart.NewWriter(multipartBuffer)
+	multipartBuffer := &bytes.Buffer{}
+	multipartWriter := multipart.NewWriter(multipartBuffer)
 
-		fmt.Fprintf(buffer, "Date: %s\r\n", time.Now().Format(time.RFC1123Z))
-		fmt.Fprintf(buffer, "Content-Type: multipart/alternative;  boundary=%s\r\n", multipartWriter.Boundary())
-		fmt.Fprintf(buffer, "MIME-Version: 1.0\r\n\r\n")
+	fmt.Fprintf(buffer, "Date: %s\r\n", time.Now().Format(time.RFC1123Z))
+	fmt.Fprintf(buffer, "Content-Type: multipart/alternative;  boundary=%s\r\n", multipartWriter.Boundary())
+	fmt.Fprintf(buffer, "MIME-Version: 1.0\r\n\r\n")
 
-		// TODO: Add some useful headers here, such as URL of the alertmanager
-		// and active/resolved.
-		_, err = message.Write(buffer.Bytes())
-		if err != nil {
-			return false, errors.Wrap(err, "write headers")
-		}
+	_, err = message.Write(buffer.Bytes())
+	if err != nil {
+		return false, errors.Wrap(err, "write headers")
+	}
 
-		if len(n.conf.Text) > 0 {
-			// Text template
-			w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
-				"Content-Transfer-Encoding": {"quoted-printable"},
-				"Content-Type":              {"text/plain; charset=UTF-8"},
-			})
-			if err != nil {
-				return false, errors.Wrap(err, "create part for text template")
-			}
-			body, err := n.tmpl.ExecuteTextString(n.conf.Text, data)
-			if err != nil {
-				return false, errors.Wrap(err, "execute text template")
-			}
-			qw := quotedprintable.NewWriter(w)
-			_, err = qw.Write([]byte(body))
-			if err != nil {
-				return true, errors.Wrap(err, "write text part")
-			}
-			err = qw.Close()
-			if err != nil {
-				return true, errors.Wrap(err, "close text part")
-			}
-		}
+	// Html template
+	// Preferred alternative placed last per section 5.1.4 of RFC 2046
+	// https://www.ietf.org/rfc/rfc2046.txt
+	w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
+		"Content-Transfer-Encoding": {"quoted-printable"},
+		"Content-Type":              {"text/html; charset=UTF-8"},
+	})
+	if err != nil {
+		return false, errors.Wrap(err, "create part for html template")
+	}
+	body, err := n.tmpl.ExpandEmail(cmd.Template+".html", cmd.Data)
+	if err != nil {
+		return false, errors.Wrap(err, "execute html template")
+	}
+	qw := quotedprintable.NewWriter(w)
+	_, err = qw.Write([]byte(body))
+	if err != nil {
+		return true, errors.Wrap(err, "write HTML part")
+	}
+	err = qw.Close()
+	if err != nil {
+		return true, errors.Wrap(err, "close HTML part")
+	}
 
-		if len(n.conf.HTML) > 0 {
-			// Html template
-			// Preferred alternative placed last per section 5.1.4 of RFC 2046
-			// https://www.ietf.org/rfc/rfc2046.txt
-			w, err := multipartWriter.CreatePart(textproto.MIMEHeader{
-				"Content-Transfer-Encoding": {"quoted-printable"},
-				"Content-Type":              {"text/html; charset=UTF-8"},
-			})
-			if err != nil {
-				return false, errors.Wrap(err, "create part for html template")
-			}
-			body, err := n.tmpl.ExecuteHTMLString(n.conf.HTML, data)
-			if err != nil {
-				return false, errors.Wrap(err, "execute html template")
-			}
-			qw := quotedprintable.NewWriter(w)
-			_, err = qw.Write([]byte(body))
-			if err != nil {
-				return true, errors.Wrap(err, "write HTML part")
-			}
-			err = qw.Close()
-			if err != nil {
-				return true, errors.Wrap(err, "close HTML part")
-			}
-		}
+	err = multipartWriter.Close()
+	if err != nil {
+		return false, errors.Wrap(err, "close multipartWriter")
+	}
 
-		err = multipartWriter.Close()
-		if err != nil {
-			return false, errors.Wrap(err, "close multipartWriter")
-		}
+	_, err = message.Write(multipartBuffer.Bytes())
+	if err != nil {
+		return false, errors.Wrap(err, "write body buffer")
+	}
 
-		_, err = message.Write(multipartBuffer.Bytes())
-		if err != nil {
-			return false, errors.Wrap(err, "write body buffer")
-		}
-
-		success = true
-		return false, nil
-	*/
+	success = true
+	return false, nil
 }
